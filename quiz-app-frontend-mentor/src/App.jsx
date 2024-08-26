0173d3722dd2497cce221919060594294860fd71
Daniel Dantas <daniel.dantas808@gmail.com>
2024-08-26 11:00:31 -0300

    feat: estrutura básica iniciada

--- quiz-app-frontend-mentor/src/App.jsx
+++ quiz-app-frontend-mentor/src/App.jsx
@@ -1,9 +1,13 @@
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
+
+
 
 function App() {
 
   return (
     <>
-      <h2>Ola mundo</h2>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ quiz-app-frontend-mentor/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Home(){
+    return (
+        <h3>Ola eu sou a home</h3>
+    )
+}
